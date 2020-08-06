47b2ce83e1a5c3be764c1123410cf44c442a8600
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-05 19:31:54 -0700

    scaffold the controllers

---
+++ src/resources/product/product.controller.js
@@ -0,0 +1,15 @@
+const { Product } = require("./product.model");
+
+const getMany = (req, res) => {};
+const getOne = (req, res) => {};
+const create = (req, res) => {};
+const update = (req, res) => {};
+const remove = (req, res) => {};
+
+module.exports = {
+	getMany,
+	getOne,
+	create,
+	update,
+	remove,
+};
---
+++ src/resources/user/user.contoller.js
@@ -0,0 +1,15 @@
+const { User } = require("./user.model");
+
+const getMany = (req, res) => {};
+const getOne = (req, res) => {};
+const create = (req, res) => {};
+const update = (req, res) => {};
+const remove = (req, res) => {};
+
+module.exports = {
+	getMany,
+	getOne,
+	create,
+	update,
+	remove,
+};
