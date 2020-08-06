dff9fafc3075630984cb155e49473f529186402b
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-05 18:58:31 -0700

    adjust the product model

--- src/resources/product/product.model.js
+++ src/resources/product/product.model.js
@@ -1,15 +1,20 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
+	productId: {
+		type: Number,
+		required: true,
+	},
 	name: {
 		type: String,
 		required: true,
 	},
 	currentPrice: {
 		type: { value: Number, curreny_code: String },
+		required: true,
 	},
 });
 
 module.exports = {
-	Product: mongoose.model("product", productSchema),
+	Product: mongoose.model("products", productSchema),
 };
