d10156c8169e25f036229827754b3c15aedb8250
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-06 15:29:23 -0700

    refactor the "getOne" controller on the "Product" type

--- src/resources/product/product.controller.js
+++ src/resources/product/product.controller.js
@@ -15,29 +15,26 @@ const getOne = (req, res) => {
 		.get(
 			`https://redsky.target.com/v3/pdp/tcin/${req.params.id}?excludes=taxonomy,price,promotion,bulk_ship,rating_and_review_reviews,rating_and_review_statistics,question_answer_statistics&key=candidate`
 		)
-		.then(
-			async function onResolve(response) {
-				const existingCopyOfProduct = await Product.findOne({
-					productId: req.params.id,
-				})
-					.lean()
-					.exec();
-				// if it's in our copy of the database
-				if (existingCopyOfProduct) {
-					res.status(200).json({ data: existingCopyOfProduct });
-				} else {
-					// else save it to our copy of the database
-					const newCopyOfProdcut = await Product.create(
-						parseToProductSchema(response)
-					);
-					res.status(200).json({ data: newCopyOfProdcut });
-				}
-			},
-			function onReject(error) {
-				console.error(error);
-				res.status(400).end(); // Bad request
+		.then(async function onResolve(response) {
+			const existingCopyOfProduct = await Product.findOne({
+				productId: req.params.id,
+			})
+				.lean()
+				.exec();
+
+			if (existingCopyOfProduct) {
+				res.status(200).json({ data: existingCopyOfProduct });
+			} else {
+				const newCopyOfProdcut = await Product.create(
+					parseToProductSchema(response)
+				);
+				res.status(200).json({ data: newCopyOfProdcut });
 			}
-		);
+		})
+		.catch(function onReject(error) {
+			console.error(error);
+			res.status(400).end(); // Bad request
+		});
 };
 const create = async (req, res) => {};
 const update = async (req, res) => {};
