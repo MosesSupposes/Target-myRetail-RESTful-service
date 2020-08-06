25953729099b67b082670dd06c82602691e48715
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-06 16:34:41 -0700

    validate required fields on the "create" controller on the "user" type

--- src/resources/user/user.router.js
+++ src/resources/user/user.router.js
@@ -1,10 +1,17 @@
 const { Router } = require("express");
 const controller = require("./user.contoller");
+const { validateFieldsShallowly } = require("../../middleware/validateFields");
 
 const router = Router();
 
 // /api/users
-router.route("/").get(controller.getMany).post(controller.create);
+router
+	.route("/")
+	.get(controller.getMany)
+	.post(
+		validateFieldsShallowly(["fullName", "email", "password"]),
+		controller.create
+	);
 
 // /api/users/:id
 router
