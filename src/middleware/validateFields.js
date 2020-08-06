1389343f74fa96246a890e5645b7178efdbfe034
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-05 18:32:40 -0700

    add a middleware that validates fields on a request's body

---
+++ src/middleware/validateFields.js
@@ -0,0 +1,19 @@
+const validateFieldsShallowly = fields => (req, res, next) => {
+	const allFieldsAreValid = fields.every(field =>
+		eval("req.body." + field + " !== undefined")
+	);
+
+	if (allFieldsAreValid) {
+		next();
+	} else {
+		const error = new Error(
+			"Your request body is missing one of these fields: " + fields.join(", ")
+		);
+		error.status = 400;
+		next(error);
+	}
+};
+
+module.exports = {
+	validateFieldsShallowly,
+};
