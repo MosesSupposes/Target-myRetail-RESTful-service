9fd39d66209b85f67b89f32c7cf549db011b2b45
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-06 13:38:37 -0700

    add a new field ("productDescription") to the Product schema

--- src/resources/product/product.model.js
+++ src/resources/product/product.model.js
@@ -9,6 +9,10 @@ const productSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 	},
+	productDescription: {
+		type: String,
+		required: true,
+	},
 	buyUrl: {
 		type: String,
 		required: true,
