a103eca3057fbd8e9e45bec2584116a0fbef0eea
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-07 12:07:21 -0700

    add a "login" controller

--- src/resources/user/user.contoller.js
+++ src/resources/user/user.contoller.js
@@ -1,7 +1,8 @@
 const { User } = require("./user.model");
 const bcrypt = require("bcrypt");
-const { omit } = require("../../utils");
-const jwtSecret = process.env.JWT_SECRET;
+const { omit, withCatch } = require("../../utils");
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET || "this is not secret";
 
 const getMany = (req, res) => {};
 
@@ -26,11 +27,48 @@ const register = (req, res, next) => {
 	});
 };
 
-const login = (req, res) => {};
+const login = async (req, res, next) => {
+	const [err, user] = await withCatch(
+		User.findOne({ _id: req.body.id }).lean().exec()
+	);
+
+	if (err || !user) {
+		res.status(400).json({ error: { message: "Invalid user ID." } });
+	} else {
+		bcrypt.compare(req.body.password, user.password, (err, passwordsMatch) => {
+			if (err) {
+				next(err);
+			} else if (!passwordsMatch) {
+				res.status(400).json({ error: { message: "Invalid password." } });
+			} else {
+				const userWithoutPassword = omit(["password"], user);
+				res.status(200).json({
+					success: `Welcome ${user.fullName}!`,
+					userWithoutPassword,
+					token: generateToken(user),
+				});
+			}
+		});
+	}
+};
 
 const update = (req, res) => {};
 
 const remove = (req, res) => {};
+
+/**
+ * Helpers
+ */
+
+function generateToken(user) {
+	const payload = {
+		fullName: user.fullName,
+		email: user.email,
+		subject: user._id,
+	};
+	const options = { expiresIn: "3h" };
+	return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = {
 	getMany,
