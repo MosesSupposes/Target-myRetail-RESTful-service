3280db2b9fef80dac44397f85f94ca59eac7de63
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-07 11:18:28 -0700

    add a "register" controller

--- src/resources/user/user.contoller.js
+++ src/resources/user/user.contoller.js
@@ -1,15 +1,40 @@
 const { User } = require("./user.model");
+const bcrypt = require("bcrypt");
+const jwtSecret = process.env.JWT_SECRET;
 
 const getMany = (req, res) => {};
+
 const getOne = (req, res) => {};
-const create = (req, res) => {};
+
+const register = (req, res, next) => {
+	bcrypt.hash(req.body.password, 8, async (err, encryptedPassword) => {
+		if (err) {
+			console.error(err);
+			next(err);
+		} else {
+			User.create({ ...req.body, password: encryptedPassword })
+				.then(newUser => {
+					res.status(200).json({ data: newUser });
+				})
+				.catch(err => {
+					console.error(err);
+					next(err);
+				});
+		}
+	});
+};
+
+const login = (req, res) => {};
+
 const update = (req, res) => {};
+
 const remove = (req, res) => {};
 
 module.exports = {
 	getMany,
 	getOne,
-	create,
+	register,
+	login,
 	update,
 	remove,
 };
