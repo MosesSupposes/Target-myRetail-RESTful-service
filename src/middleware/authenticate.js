9a8d6d8cf0d600cb0ac2b1d56066a7c3c2b5043f
Moses A. Samuel <mosamuel98@gmail.com>
2020-08-07 17:54:14 -0700

    add a middleware for authenticating users

---
+++ src/middleware/authenticate.js
@@ -0,0 +1,32 @@
+const jwt = require("jsonwebtoken");
+const jwtSecret = process.env.JWT_SECRET || "this is not secret";
+
+const authenticate = (req, res, next) => {
+	const token = req.headers.authorization;
+
+	if (token) {
+		jwt.verify(token, jwtSecret, (err, decodedToken) => {
+			if (err) {
+				const error = new Error("Invalid token.");
+				error.status = 400;
+				next(error);
+			} else {
+				req.user = {
+					email: decodedToken.email,
+					fullName: decodedToken.fullName,
+				};
+				next();
+			}
+		});
+	} else {
+		const error = new Error(
+			"You need a token tto access this resource. Obtain one by logging in or registering an account."
+		);
+		error.stattus = 400;
+		next(error);
+	}
+};
+
+module.exports = {
+	authenticate,
+};
